a294c748dd91c21a8bcd5f57ce18750a01a9e4c0
arminsafie <arminsafaie92@gmail.com>
2024-05-15 13:02:03 +0330

    bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,6 @@ module.exports = {
       transitionDuration: {
         ".8s": ".8s",
       },
-      animation: {
-        ping: "ping 2s cubic-bezier(0, 0, 0.2, 1) infinite ",
-      },
       container: {
         center: true,
       },
